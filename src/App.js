c94a7f203841f873d4a42d9a23946492be21856e
Jerome Yang <cryang1018@gmail.com>
2018-09-03 16:44:58 -0400

    Update personal info

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Card from './Card';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import styled from "styled-components";
+import { FaGithub, FaTwitter, FaFacebook, FaLinkedin, FaEnvelope, FaBlogger } from 'react-icons/fa';
+
+const Wallpaper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  a {
+    color: #000;
+    text-decoration: none;
+    margin: 0 5px;
+  }
+`;
 
 const App = () => {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      <Card />
-    </div>
+    <Wallpaper>
+      <Typography variant="display4" align="center" color="textPrimary" gutterBottom>
+        Jerome Yang
+      </Typography>
+      <Typography variant="title" align="center" color="textSecondary" paragraph>
+        UI/UX Engineering · Maps & DataViz · Culture & Language · Travel
+      </Typography>
+      <Typography variant="title" align="center" color="textPrimary">
+        <a href="mailto:hi@jeromeyang.com" target="_blank"><FaEnvelope /></a>
+        <a href="https://github.com/jeromecyang" target="_blank"><FaGithub /></a>
+        <a href="https://twitter.com/jeromyang" target="_blank"><FaTwitter /></a>
+        <a href="https://www.facebook.com/jeromeyangencounters" target="_blank"><FaFacebook /></a>
+        <a href="https://www.linkedin.com/in/jerome-yang-91995a4b" target="_blank"><FaLinkedin /></a>
+        <a href="http://blog.jeromeyang.com/" target="_blank"><FaBlogger /></a>
+      </Typography>
+    </Wallpaper>
   );
 }
 
