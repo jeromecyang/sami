6b64ccd0cb6c3d939b7cde871c5caf3c741b5a3a
Jerome Yang <cryang1018@gmail.com>
2018-12-19 20:30:43 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -34,10 +34,7 @@ const App = () => {
     <Master>
       <Wallpaper>
         <Typography variant="display4" align="center" color="textPrimary">
-          Jerome Yang
-        </Typography>
-        <Typography variant="title" align="center" color="textSecondary" paragraph>
-          UI/UX Engineering · Maps & DataViz · Culture & Language · Travel
+          Lifetime Sojourner
         </Typography>
         <Typography variant="title" align="center" color="textPrimary">
           <a href="mailto:hi@jeromeyang.com" target="_blank"><FaEnvelope /></a>
@@ -48,35 +45,37 @@ const App = () => {
           <a href="https://www.youtube.com/channel/UCsofLyXgW1Ra1Rg4-38tbgg" target="_blank"><FaYoutube /></a>
           <a href="http://blog.jeromeyang.com/" target="_blank"><FaBlogger /></a>
         </Typography>
+        <Typography variant="title" align="center" color="textSecondary" paragraph>
+          Jerome Yang
+        </Typography>
       </Wallpaper>
+
       <Section>
+      </Section>
         <Typography align="center" variant="display3" paragraph>about</Typography>
-        <Typography paragraph>
-          I'm a software engineer, traveler and blogger based in Boston, MA.
-        </Typography>
-        <Typography paragraph>
-          As a software engineer, I write code, but not for the sake of writing code. I'm convicted that software programming is nothing but a tool that helps people solve their everyday problems, and the problems that I'm interested to solve are: data manipulation and visualization, maps and geospatial information, linguistics and communication.
-        </Typography>
-        <Typography paragraph>
-          As a traveler, I have visited 21 countries, including several less traveled ones such as Latvia, Estonia, Uzbekistan and Kyrgyzstan. Travels are an important projects for me, in which I encounter people, culture, history and reality.
-        </Typography>
-        <Typography paragraph>
-          As a blogger, I write articles.
-        </Typography>
-        <Typography align="center" paragraph>
-          <img src="https://www.hubspot.com/hubfs/HubSpot_Logos/HSLogo_color.svg?t=1536803881422" style={{ 'vertical-align': 'middle' }} />
-          <img src="https://static1.squarespace.com/static/595cf799099c0165c82db072/t/595cf7e16b8f5ba9a838f432/1536596881878/" style={{ filter: 'invert(100%)', width: '150px', 'vertical-align': 'middle' }} />
-        </Typography>
+      <Section>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6} align="center">
+            <img src="./pic.jpg" style={{ 'width': '280px' }} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography paragraph>
+              I'm a software engineer, traveler and blogger based in Boston, MA.
+            </Typography>
+            <Typography paragraph>
+              As a software engineer, I write code, but not for the sake of writing code. I'm convicted that software programming is nothing but a tool that helps people solve their everyday problems, and the problems that I'm interested to solve are: data manipulation and visualization, maps and geospatial information, linguistics and communication.
+            </Typography>
+            <Typography paragraph>
+              As a traveler, I have visited 21 countries, including several less traveled ones such as Latvia, Estonia, Uzbekistan and Kyrgyzstan. Travels are an important projects for me, in which I encounter people, culture, history and reality.
+            </Typography>
+            <Typography paragraph>
+              As a blogger, I write articles.
+            </Typography>
+          </Grid>
+        </Grid>
       </Section>
       <Section>
         <Typography align="center" variant="display3" paragraph>stories</Typography>
-        <Grid container justify="center" spacing={16}>
-          {new Array(16).fill(0).map(() => (
-            <Grid item>
-              <Card></Card>
-            </Grid>
-          ))}
-        </Grid>
       </Section>
       <Section>
         <Typography align="center" variant="display3">connect</Typography>
